1eb1199fd4735cdcaac9ecd9a63c47c9a1f76ccd
Petr Komarek (Europe) <petr.komarek@global.ntt>
2024-04-26 14:58:45 +0200

    0.0.0-alpha1 Initial version with `RepositoryEfCore` implementation

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Repository pattern
+
+Repository pattern implementation focused on EF Core.
+
+## Versions
+
+### 0.0.0-alpha1
+Initial version with `RepositoryEfCore` implementation
---
+++ RepositoryPattern/Repository/IIdentifiable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    /// <summary>
+    /// Class with identifier of the type <see cref="T"/>
+    /// Typical is int or string
+    /// </summary>
+    /// <typeparam name="T">Datatype of the identyfier</typeparam>
+    public  interface IIdentifiable<T>
+    {
+        T Id { get; }
+    }
+}
